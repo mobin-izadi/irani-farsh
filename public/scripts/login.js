850e7203511ab0ddbf438e97416e746d8b787333
mobin <mobinizadi.dev@gmail.com>
2025-02-24 21:06:23 +0330

    Fix the input pass on the login page

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,10 +1,35 @@
 let userNameInput = document.querySelector('#username')
 let labelUserName = document.querySelector('#username-label')
-
+let passwordInput = document.querySelector('#password')
+let labelPassword = document.querySelector('#password-label')
+let passwordStatusButton = document.querySelector('#pass-status-btn')
+let eyeOffSvg = document.querySelector('#eye-off-icon')
+let eyeSvg = document.querySelector('#eye-icon')
+// Check that we have a value in the user name input
 userNameInput.addEventListener('keyup', event => {
     if (event.target.value.length > 0) {
         labelUserName.classList.add('-top-3')
-
+    } else {
+        labelUserName.classList.remove('-top-3')
     }
-
 })
+// Check that we have a value in the password input
+passwordInput.addEventListener('keyup', event => {
+    if (event.target.value.length > 0) {
+        labelPassword.classList.add('-top-3')
+    } else {
+        labelPassword.classList.remove('-top-3')
+    }
+})
+// password show & hidden
+passwordStatusButton.addEventListener('click', event => {
+    event.preventDefault()
+    eyeOffSvg.classList.toggle('hidden')
+    eyeSvg.classList.toggle('hidden')
+    if (eyeOffSvg.classList.contains('hidden')) {
+        passwordInput.type = 'text'
+    } else {
+        passwordInput.type = 'password'
+    }
+})
+
